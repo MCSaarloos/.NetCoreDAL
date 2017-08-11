214e9ac153c24d6be70d2e1c233cbebac91037a1
marcel.saarloos@avanade.com <marcel.saarloos@avanade.com>
2017-08-11 09:40:52 +0200

    added add example

--- NetCoreDAL.Business/Logics/SettingsLogic.cs
+++ NetCoreDAL.Business/Logics/SettingsLogic.cs
@@ -2,13 +2,14 @@
 using NetCoreDAL.DataAccess.Infrastructure.Interfaces;
 using NetCoreDAL.Domain.Entities;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace NetCoreDAL.Business.Logics
 {
     public class SettingsLogic : ISettingsLogic
     {
         private readonly IUnitOfWork _unitOfWork;
-        private readonly IReadRepository<Settings> _repository;
+        private readonly IRepository<Settings> _repository;
 
         public SettingsLogic(IUnitOfWork unitOfWork)
         {
@@ -19,6 +20,12 @@ namespace NetCoreDAL.Business.Logics
         public IEnumerable<Settings> GetAllSettings()
         {
             return _repository.GetAll();
+        }
+
+        public async Task<bool> SaveSettings(Settings settings)
+        {
+            _repository.Add(settings);
+            return await _unitOfWork.SaveChanges();
         }
     }
 }
--- NetCoreDAL.Businesss.Infrastructure/Interfaces/ISettingsLogic.cs
+++ NetCoreDAL.Businesss.Infrastructure/Interfaces/ISettingsLogic.cs
@@ -1,10 +1,12 @@
 ﻿using NetCoreDAL.Domain.Entities;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace NetCoreDAL.Businesss.Infrastructure.Interfaces
 {
     public interface ISettingsLogic
     {
         IEnumerable<Settings> GetAllSettings();
+        Task<bool> SaveSettings(Settings settings);
     }
 }
